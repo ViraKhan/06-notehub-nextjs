023b4ac380530cd3537a246c02b3491d2797d9a6
Vira Khan <vira.khan1@gmail.com>
2025-10-02 17:17:27 +0200

    fix name noteDetails.client

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -3,7 +3,7 @@ import {
   HydrationBoundary,
   QueryClient,
 } from "@tanstack/react-query";
-import NoteDetailsClient from "./NoteDetails";
+import NoteDetailsClient from "./NoteDetails.client";
 import { fetchNoteById } from "@/lib/api";
 
 interface NoteDetailsProps {
